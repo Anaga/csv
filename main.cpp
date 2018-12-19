12b25735e49cbf5ace41db511dfae57a15381c43
Aleksandr Bobyrev <bobyrev.bob@gmail.com>
2018-12-19 14:57:03 +0200

    add output files. comment debug output

--- main.cpp
+++ main.cpp
@@ -12,15 +12,15 @@ void process_line(QString input)
         double lux = qsl.at(0).toDouble();
         QString qsTemp = QString::number(lux,'f',6);
         qsl[0]=qsTemp;
-        qDebug() << "QSL at 1 :" << qsl.at(1);
+        //qDebug() << "QSL at 1 :" << qsl.at(1);
         qint64 unixTimeMilSec = static_cast<qint64>(qsl.at(1).toLongLong());
-        qint64 unixTimeSec = (unixTimeMilSec/ static_cast<qint64>(1000));
+        //qint64 unixTimeSec = (unixTimeMilSec/ static_cast<qint64>(1000));
         QDateTime dt =QDateTime::fromMSecsSinceEpoch(unixTimeMilSec);
         qsTemp = dt.toString();
-        qDebug() << "Lux :" << lux << "unixTimeSec" << unixTimeSec << "dt: " << qsTemp << "Current hour: " << dt.time().hour();
+        //qDebug() << "Lux :" << lux << "unixTimeSec" << unixTimeSec << "dt: " << qsTemp << "Current hour: " << dt.time().hour();
         qsl.append(qsTemp);
         for (int i = 0; i < qsl.size(); ++i) {
-            qDebug() << "QSL at "<<i << " : "<< qsl.at(i);
+            //qDebug() << "QSL at "<<i << " : "<< qsl.at(i);
         }
         QString outStr = qsl.join(';');
         outStr.append("\n");
